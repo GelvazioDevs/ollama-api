54e9026c31990dd392efac36104a45ff5e803b6a
GelvazioDev <gelvazio@hotmail.com>
2024-12-04 17:45:10 -0300

    ok

---
+++ Readme.md
@@ -0,0 +1,7 @@
+### Api de integracao ollama
+* lib usada:
+* https://day.js.org/docs/en/installation/node-js
+
+
+### Video de uso da IA
+* https://www.youtube.com/watch?v=-tR9-gLLE9w
--- index.js
+++ index.js
@@ -1,18 +1,41 @@
-import express from "express";
 import axios from "axios";
+import dayjs from 'dayjs';
+import express from "express";
 
+const version = "v1";
 const app = express();
+
 app.use(express.json());
 
-app.post("/ia", async (req, res) => {
+app.get("/api/" + version  + "/ping", async(req, res) => {
+  const { text } = req.body;
+  
+  const respData = {
+    data:dayjs().format('2024-12-04'),
+    input:text.toString()
+  };
+
+  res.send(respData);
+});
+
+app.get("/api/" + version  + "/tags", async(req, res) => {
+  const response = await axios.get("http://localhost:11434/api/tags");
+  res.send(JSON.stringify(response));
+});
+
+app.post("/api/" + version  + "/ia", async (req, res) => {
   const { text } = req.body;
   const response = await axios.post("http://localhost:11434/api/generate", {
     model: "llama3",
     prompt: text,
     stream: false,
   });
-  const respData = response.data.response.toString()
-  res.send(respData);
+  // const respData = response.data.response.toString()
+  
+  // res.send(respData);
+  //res.send(JSON.stringify(response.data.response));
+
+  return res.json(JSON.stringify(response.data))
 });
 
 app.listen(3000, () => {
